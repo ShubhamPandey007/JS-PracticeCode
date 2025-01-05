278290b4e6b8d09be85799e8f3385c36cc1a2cad
shubham <shubhampandeyup07@gmail.com>
2025-01-06 00:03:42 +0530

    js code

---
+++ New folder/script1.js
@@ -0,0 +1,30 @@
+for(let i=0;i<10;i++); {console.log("Hii");}
+
+for(let i=0;i<10;i++)
+ {console.log("Hii");}
+
+ //
+
+ 
+for(let i=0;i<3;i++){
+    console.log( "i", i)
+
+    for (let j=0;j<3;j++){
+        console.log("j", j)
+        if(j==1){
+            break;
+        }
+    }
+}
+ ///
+
+ 
+for(let i = 0 ;i<4;i++){
+    console.log(i);
+    if(i==2){
+        continue;
+    }
+    console.log(i*20)
+}
+
+
